188b096930debf267e1c42304f4de8767e4e3af2
Rafael S. de Souza <rafael.2706@gmail.com>
2017-09-27 09:49:14 -0400

    d

--- RNA_heatmap.R
+++ RNA_heatmap.R
@@ -2,6 +2,7 @@ library(gplots)
 library(Heatplus)
 library(vegan)
 library(RColorBrewer)
+library(circlize)
 
 all.data <- read.csv("grant.csv")
 
@@ -13,23 +14,76 @@ colnames(all.data) <- c("EV","shPDK1","shPDK1_2")
 
 heatmap(as.matrix(all.data), Rowv = NA, Colv = NA)
 
-data.dist <- vegdist(all.data, method = "bray")
+data.dist <- vegdist(all.data, method = "euclidean")
 row.clus <- hclust(data.dist, "aver")
 heatmap(as.matrix(all.data), Rowv = as.dendrogram(row.clus), Colv = NA, margins = c(10, 5))
 
 
 
 
-
+pdf("heatmap.pdf",height=10,width=9)
 plot(annHeatmap2(as.matrix(all.data),
+col = colorRampPalette(c("#d73027", "#fdae61","#ffffbf","#abd9e9","#2c7bb6"),
+                       space = "rgb")(24), breaks = 24,
+dendrogram = list(Row = list(dendro = as.dendrogram(row.clus))), legend = 1, 
+labels = list(Col = list(nrow = 10))))
+dev.off()
 
-                 # the colours have to be fiddled with a little more than with the other functions. With 50 breaks in the heatmap densities, there needs to be 51 colours in the scale palette. Error messages from the plotting should help to determine how many colours are needed for a given number of breaks
 
-                 col = colorRampPalette(c("blue2", "red2"), space = "rgb")(24), breaks = 24,
 
-                 # dendrogram controls how dendrograms are made and can be calculatated witin the function. The internal calculation can be overridden and externally calculated dendrograms can be displayed.
+gg <- all.data
+gg$class <- row.names(all.data)
+chordDiagram(as.matrix(all.data),directional = 1,direction.type="arrows")
+mat <- as.matrix(all.data)
 
-                 dendrogram = list(Row = list(dendro = as.dendrogram(row.clus))), legend = 3, # this puts the colour-scale legend on the plot. The number indicates the side on which to plot it (1 = bottom, 2 = left, 3 = top, 4 = right)
+df = data.frame(from = rep(rownames(mat), times = ncol(mat)),
+                to = rep(colnames(mat), each = nrow(mat)),
+                value = as.vector(mat),
+                stringsAsFactors = FALSE)
+grid.col = c(EV = "grey", shPDK1 = "grey", shPDK1_2 = "grey",rep("grey",25))
+col_fun = colorRamp2(range(mat), c("#FFEEEE", "#FF0000"), transparency = 0.5)
+chordDiagram(mat,  grid.col = "grey",column.col = c("red","blue","green"))
 
-                 labels = list(Col = list(nrow = 12)) # gives more space for the Genus names
-                 ))
+
+#tested for i in [1:5]
+i=1.25
+
+#--- Define par parameters ---
+#par(mar = rep(0,4), cex = 2)
+par(mar=rep(i*2,4), mgp=c(i*3,i*2, i*1.5), las=2,cex=0.45*i)
+
+chordDiagram(mat,  grid.col = "grey",column.col = c("red","blue","green"),
+direction.type = c("diffHeight", "arrows"),
+link.arr.col = "grey", link.arr.length = 0.2,annotationTrack = "grid", preAllocateTracks = 1)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.75))
+    circos.axis(h = "top", labels.cex = 1, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+
+circos.clear()
+
+
+us.dend <- hclust(dist(scale(mat)))
+
+EV <- mat[, 'EV']
+shPDK1 <- mat[, 'shPDK1']
+shPDK1_2 <- mat[, 'shPDK1_2']
+
+## generate color maps
+EV.cmap <- makecmap(EV, n = 25,colFn = jet,breaks = prettyLog)
+shPDK1.cmap <- makecmap(shPDK1, n = 25, colFn = jet,breaks = prettyLog)
+shPDK1_2.cmap <- makecmap(shPDK1_2, n = 25, colFn = jet,breaks = prettyLog )
+
+us.mat <- data.frame(shPDK1_2 = cmap(shPDK1_2, EV.cmap ),
+                     shPDK1 = cmap(shPDK1, EV.cmap ),
+                     EV = cmap(EV, EV.cmap))
+
+par(mar = c(4,5,4,4)+0.1)  # make space for color keys
+dendromat(us.dend, us.mat)
+
+#vkey(shPDK1_2.cmap, 'shPDK1_2',y=-0.1)
+vkey(shPDK1.cmap, '',y=0.35)
+#vkey(EV.cmap, 'EV',y=0.75)
